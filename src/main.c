7df9e8f1a859685cec182c9bb9fbcf9f5b13f120
ViniciusTei <viniciustprates@gmail.com>
2022-03-22 09:56:09 -0300

    read file name from args

--- src/main.c
+++ src/main.c
@@ -1,15 +1,30 @@
 #include "../includes/CasamentoCaracteres/CasamentoCaracteres.h"
 #include "../includes/Decoder/Decoder.h"
 
-int main(void) {
+int main(int argc, char **argv) {
   ArrayList decoderList;
   ArrayList tabelaFreqPortBR;
   Text text;
   int controle;
   int in;
   char *codigo;
+  char nome_arquivo[100];
+  
+  // quando o nome do arquivo eh passado como argumento iniciamos nossa variavel
+  if (argc > 0) {
+    for (int i = 0; i < argc; i++) {
+      if (strcmp(argv[i], "--file") == 0 || strcmp(argv[i], "--arquivo") == 0) {
+        strcpy(nome_arquivo, argv[i+1]);
+      }
+    }
+  } 
 
-  codigo = readEncryptedTextFromFile("texto-criptografado.txt");
+  if (strlen(nome_arquivo) == 0) {
+    printf("\nVoce nao passou um arquivo de entrada! O programa sera fechado\n");
+    exit(1);
+  }
+
+  codigo = readEncryptedTextFromFile(nome_arquivo);
 
   initText(&text, codigo);
 
